a5565b641981f6930b9ecf3ffb92cd11902d011a
Oussama Fradi <of@whitecapetech.local>
2024-09-19 12:23:54 +0100

    zoneOu

--- src/app/components/adavanced-search/adavanced-search.component.ts
+++ src/app/components/adavanced-search/adavanced-search.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Structure } from 'src/app/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { combineLatest, finalize, tap } from 'rxjs';
+import { combineLatest, finalize, first, tap } from 'rxjs';
 import { AdvancedSearchService } from 'src/app/services/advanced-search/advanced-search.service';
 
 @Component({
@@ -12,18 +14,32 @@ export class AdavancedSearchComponent implements OnInit {
 
   displayedColumns: string[] = ['profil', 'identite', 'specialite', 'structure', 'localisation', 'distance', 'actions'];
   dataSource: MatTableDataSource<any>;
-  sortBy = 'firstName';
+  data: any[] = [];
+  sortBy = 'lastName';
   isLoading = true;
+  chips: string[] = [];
+  showHospitals: boolean = true;
+  showReceivers: boolean = true;
+  showPractitioners: boolean = true;
+  showDetail: boolean = false;
+  dataPerson = {
+    firstName: "",
+    lastName: "",
+    speciality: "",
+    rpps: "",
+    phone: "",
+    location: "",
+    email: ""
+  };
+  whoAreaUsed: boolean = true;
+  whereAreaUsed: boolean = false;
+  sectionUsed: string = "";
+  combinedDatas: any;
+
+  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
 
   constructor(private advancedSearchService: AdvancedSearchService) {
-    const data = [
-      { identite: 'Hôpital de Morvan', specialite: '+9 spécialités disponibles', structure: 'CHRU', localisation: '29 Brest', distance: '137 km' },
-      { identite: 'ABGRAL Karim', specialite: 'Cardiologie', structure: 'Hôpital de Morvan', localisation: '29 Brest', distance: '232 km' },
-      { identite: 'FERRARI Gisèle', specialite: 'Médecine Interne', structure: 'Hôpital de Morvan', localisation: '29 Brest', distance: '137 km' },
-      { identite: 'GAUGUE Vanessa', specialite: 'Médecine Polyvalente', structure: 'Hôpital de Morvan', localisation: '29 Brest', distance: '137 km' },
-      { identite: 'VALOGNE Hadia', specialite: 'Gériatrie', structure: 'Hôpital de Morvan', localisation: '29 Brest', distance: '232 km' },
-    ];
-    this.dataSource = new MatTableDataSource(data);
+    this.dataSource = new MatTableDataSource(this.combinedDatas);
   }
 
   ngOnInit(): void {
@@ -35,24 +51,234 @@ export class AdavancedSearchComponent implements OnInit {
       this.advancedSearchService.getPersons(),
       this.advancedSearchService.getSpecialities(),
       this.advancedSearchService.getStructures(),
+      this.advancedSearchService.getLocations(),
     ]).pipe(
       tap({
-        next: ([persons, specialities, structures]) => {
-          console.log('persons', persons);
-          console.log('specialities', specialities);
-          console.log('structures', structures);
+        next: ([persons, specialities, structures, locations]) => {
+          this.combinedDatas = [persons, specialities, structures, locations];
+
+          const combinedData = [
+            ...structures.map(structure => ({
+              profil: 'hospital',
+              identite: structure.name,
+              specialite: structure.medicalSpecialties?.length + ' spécialité(s) disponible(s)',
+              specialities: structure.medicalSpecialties,
+              structure: structure.name,
+              localisation: structure.street,
+              distance: `152 km`,
+              actions: 'View'
+            })),
+            ...persons.map(person => ({
+              profil: 'person',
+              identite: `${person.lastName} ${person.firstName} `,
+              firstName: person.firstName,
+              lastName: person.lastName,
+              specialite: person.medicalSpecialtyNames,
+              structure: person.medicalStructureName,
+              localisation: person.medicalStructurePostalCode + '' + person.medicalStructureStreet,
+              distance: `132 km`,
+              actions: 'View'
+            })),
+            ...specialities.map(speciality => ({
+              profil: 'speciality',
+              identite: speciality.name,
+              specialite: speciality.name,
+              specialiteId: speciality.medicalSpecialtyId,
+              structure: "speciality.structure",
+              localisation: " speciality.location",
+              distance: `128 km`,
+              actions: 'View'
+            })),
+
+          ];
+          this.data = combinedData;
+          this.dataSource = new MatTableDataSource(combinedData);
+          this.dataSource.paginator = this.paginator;
         }
       }),
       finalize(() => this.isLoading = false)
     ).subscribe();
   }
 
-  removeChip() {
-    console.log('Chip supprimé');
+  updateChips(chips: string[], searchTerm: string, location: string): string[] {
+    let updatedChips = [...chips];
+
+    if (searchTerm && !updatedChips.includes(searchTerm)) {
+      updatedChips = [...updatedChips, searchTerm];
+    }
+
+    if (location && !updatedChips.includes(location)) {
+      updatedChips = [...updatedChips, location];
+    }
+
+    return updatedChips;
   }
 
-  removeAllFilters() {
+  removeChip(chip: string) {
+    console.log('Chip supprimé');
+    const index = this.chips.indexOf(chip);
+    if (index >= 0) {
+      this.chips.splice(index, 1);
+    }
+    this.applyChipsFilter();
+  }
+
+  applyChipsFilter(item: any = {}): void {
+    if (this.chips.length === 0) {
+      this.dataSource.filter = '';
+      this.applySort();
+      return;
+    }
+
+    const combinedFilter = this.chips.join(' ').toLowerCase();
+
+    this.showHospitals = false;
+    this.showPractitioners = false;
+    this.showReceivers = true;
+    this.sortBy = "distance";
+    this.dataSource.filterPredicate = (data: any, filter: string) => {
+      return data.identite.toLowerCase().includes(filter) ||
+        data.specialite.toLowerCase().includes(filter) ||
+        data.structure.toLowerCase().includes(filter);
+    };
+
+    if (item.sectionUsed == "SPECIALTIE") {
+      this.showHospitals = true;
+      this.showPractitioners = false;
+      this.showReceivers = true;
+      this.sortBy = "distance"
+      this.dataSource.filterPredicate = (data: any, filter: string) => {
+
+        if (data.profil === 'hospital') {
+          if (item.whereAreaUsed) {
+            return data.specialities && data.specialities.includes(item.specialtyId) || data.localisation && data.localisation.includes(item.whereSearchText);
+          }
+          return data.specialities && data.specialities.includes(item.specialtyId);
+        }
+
+        if (data.profil === 'person') {
+          if (item.whereAreaUsed) {
+            return data.specialite && data.specialite.includes(item.whoSearchText) || data.localisation && data.localisation.includes(item.whereSearchText);
+          }
+          return data.specialite && data.specialite.includes(item.whoSearchText);
+        }
+
+        return false;
+      };
+    }
+    if (item.sectionUsed == "SDS") {
+      this.showHospitals = false;
+      this.showPractitioners = true;
+      this.showReceivers = true;
+      this.sortBy = "lastName"
+      this.dataSource.filterPredicate = (data: any, filter: string) => {
+        if (data.profil === 'person') {
+          return data.structure && data.structure.includes(item.whoSearchText);
+        }
+
+        return false;
+      };
+    }
+
+    this.dataSource.filter = combinedFilter.trim();
+    this.dataSource.paginator = this.paginator;
+    this.applySort();
+    this.showData()
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  removeAllFilters(): void {
     console.log('Supprimer tous les filtres');
+    this.chips = [];
+    this.dataSource.filter = '';
+    this.showHospitals = true;
+    this.showPractitioners = true;
+    this.showReceivers = true;
+    this.showData();
+    this.sortBy = '';
+    this.applySort();
+  }
+
+  applySort() {
+    const data = this.dataSource.data;
+    this.dataSource.data = data.sort((a, b) => {
+      switch (this.sortBy) {
+        case 'firstName':
+          if (a.profil === "person" && b.profil === "person") {
+            return this.compareStrings(a.firstName, b.firstName);
+          }
+          return 0;
+        case 'lastName':
+          if (a.profil === "person" && b.profil === "person") {
+            return this.compareStrings(a.lastName, b.lastName);
+          }
+          return 0;
+        case 'specialty':
+          return this.compareStrings(a.specialite, b.specialite);
+        case 'distance':
+          return this.compareNumbers(this.extractNumber(a.distance), this.extractNumber(b.distance));
+        default:
+          return 0;
+      }
+    });
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  compareStrings(a: string, b: string): number {
+    return a.localeCompare(b);
+  }
+
+  compareNumbers(a: number, b: number): number {
+    return a - b;
+  }
+
+  extractNumber(distance: string): number {
+    return Number(distance.split(' ')[0]);
+  }
+
+  showData() {
+    const filteredData = this.data.filter(item => {
+      if (this.showHospitals && item.profil === 'hospital') {
+        return true;
+      }
+      if (this.showReceivers && item.profil === 'person') {
+        return true;
+      }
+      if (this.showPractitioners && item.profil === 'speciality') {
+        return true;
+      }
+      return false;
+    });
+
+    this.dataSource.data = filteredData.length ? filteredData : [];
+    this.applySort();
+    this.dataSource.paginator = this.paginator;
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  getItemsFromChild(items: any) {
+    console.log(items)
+    this.showDetail = items.showDetail ? items.showDetail : false;
+    if (!this.showDetail) {
+      this.chips = this.updateChips(this.chips, items.whoSearchText, items.whereSearchText);
+    } else {
+      this.chips = [];
+      this.dataPerson = items.dataPerson ? items.dataPerson : {};
+    }
+    this.whoAreaUsed = items.whoAreaUsed;
+    this.whereAreaUsed = items.whereAreaUsed;
+    this.sectionUsed = items.sectionUsed;
+
+    this.applyChipsFilter(items);
+    console.log(this.chips)
   }
 
 }
--- src/app/components/adavanced-search/search-bar/search-bar.component.ts
+++ src/app/components/adavanced-search/search-bar/search-bar.component.ts
@@ -1,22 +1,252 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteTrigger } from '@angular/material/autocomplete';
+import { map, startWith } from 'rxjs';
+import { Person, Specialty, Structure } from 'src/app/common';
+import { Location } from 'src/app/common/models/location.model';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  styleUrls: ['./search-bar.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class SearchBarComponent implements OnInit {
 
+  searchControl = new FormControl('');
+  searchLocationControl = new FormControl('');
   searchTerm: string = '';
   location: string = '';
+  persons : Person[] = [];
+  specialities : Specialty[] = [];
+  structures : Structure[] = [];
+  locations : Location[] = [];
+  filteredPersons: any;
+  filteredSpecialities: any;
+  filteredStructures: any;
+  filteredLocation: any;
+  toHighlight: string = '';
+  //currentFilteredOptions:any;
 
-  constructor() { }
+  // Variables to track if user is typing or has selected a value
+  isTypingQui: boolean = false;
+  isTypingOu: boolean = false;
+  isQuiSelected: boolean = false;
+  isOuSelected: boolean = false;
+  isSpecialtySelected: boolean = false;
+  
+  @Input() data : any;
+  @Output() newItemEvent = new EventEmitter<any>();
+  @ViewChild(MatAutocompleteTrigger) matAutocomplete: any;
+
+  
+  constructor() { 
+
+    this.filteredPersons = this.searchControl.valueChanges.pipe(
+      startWith(''),
+      map(value => value.length >= 1 ? this._filter(value || '', this.persons, 0): []),
+    );
+
+    this.filteredSpecialities = this.searchControl.valueChanges.pipe(
+      startWith(''),
+      map(value => value.length >= 1 ? this._filter(value || '', this.specialities, 1): []),
+    );
+
+    this.filteredStructures = this.searchControl.valueChanges.pipe(
+      startWith(''),
+      map(value => value.length >= 1 ? this._filter(value || '', this.structures, 2): []),
+    );
+
+    this.filteredLocation = this.searchLocationControl.valueChanges.pipe(
+      startWith(''),
+      map(value => value.length >= 1 ? this._filterLocation(value || '', this.locations): []),
+    );
+
+    /*this.filteredPersons.subscribe((options:any) => {
+      this.currentFilteredOptions = options;
+    });*/
+
+    
+   }
 
   ngOnInit(): void {
+    this.persons = this.data[0];
+    this.specialities = this.data[1];
+    this.structures = this.data[2];
+    this.locations = this.data[3];
   }
 
+  private _filter(value: string, data : any, type : number): any[] {
+    this.toHighlight = value;
+    const filterValue = value.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+    return data.filter((option :any) => {
+      var name = '';
+      if(type == 0) {
+        name = option.firstName.concat(' '+option.lastName);
+      } else {
+        name = option.name;
+      }
+      const words = name.split(/[\s-]+/).map((part:any) => part.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, ''));
+      return words.some((word:any) => word.startsWith(filterValue))
+    });
+  }
+  
   onSearch() {
-
+    //this.newItemEvent.emit(this.currentFilteredOptions);
+      this.getData(this.searchTerm);
   }
 
+  onOptionSelected(event : any): void { 
+    const selectedValue = event.option.value;
+
+    if (selectedValue && selectedValue.hasOwnProperty('name')) {
+      this.isSpecialtySelected = true;
+      this.isQuiSelected = true;  // Something is selected
+      this.searchLocationControl.enable(); // Enable "Où" field if a specialty is selected
+    } else {
+      this.isSpecialtySelected = false;
+      this.isQuiSelected = true;
+      this.searchLocationControl.disable();
+    }
+    this.getData(selectedValue);
+  }
+
+  onEnter() {
+    if (this.matAutocomplete.panelOpen) {
+      this.matAutocomplete.closePanel(); 
+    }
+    this.onSearch();
+  }
+
+  getData(dataOption:any) {
+    var item : any = {
+      whoAreaUsed: true ,
+      whereAreaUsed: false,
+      whoSearchText: "",
+      whereSearchText: "",
+      sectionUsed: "",
+      showDetail:false,
+      dataPerson:{},
+      idSpeciality:-1,
+      selectedHospitalData: {}
+    }
+    if(typeof(dataOption) === 'string') {
+      item.whoSearchText = dataOption;
+    } else {
+      if(this.persons.includes(dataOption)) {
+        item.whoSearchText = dataOption.fullName;
+        item.sectionUsed = "PS";
+        item.showDetail = true;
+        item.dataPerson = {
+          firstName: dataOption.firstName,
+          lastName: dataOption.lastName,
+          speciality: dataOption.medicalSpecialtyNames,
+          rpps: dataOption.rpps,
+          phone: dataOption.phoneNumber,
+          location: ' ... ',
+          email: dataOption.email
+        }
+      } else if(this.specialities.includes(dataOption)) {
+          item.whoSearchText = dataOption.name;
+          item.sectionUsed = "SPECIALTIE";
+          item.idSpeciality = dataOption.medicalSpecialtyId;
+      } else {
+        item.whoSearchText = dataOption.name;
+        item.sectionUsed = "SDS",
+        item.selectedHospitalData = { medicalSpecialties: dataOption.medicalSpecialties };
+      }
+    }
+
+    //this.newItemEvent.emit(event.option.value);
+    this.newItemEvent.emit(item);
+  }
+
+  displayFn(option: any): string {
+    return option ? option.name ? option.name : option.fullName : '';
+  }
+
+  private _filterLocation(value: string, data: any): any[] {
+    this.toHighlight = value;
+    debugger;
+    // Check if the input starts with a number
+    if (!isNaN(Number(value))) {
+      // Case 1: 1 digit, return nothing
+      if (value.length === 1) {
+        return [];
+      }
+  
+      // Case 2: 2 digits, return postal code + city name with these 2 digits
+      if (value.length === 2) {
+        return data.filter((option: Location) => {
+          const postalCode = option.postalCode.toString();
+          return postalCode.startsWith(value);  // Return matching postal codes with 2 digits
+        });
+      }
+  
+      // Case 3: 3 digits, return postal code + city name with these 3 digits
+      if (value.length >= 3) {
+        return data.filter((option: Location) => {
+          const postalCode = option.postalCode.toString();
+          return postalCode.startsWith(value);  // Return matching postal codes with 3 digits
+        });
+      }
+  
+    } else {
+      // Case 4: 1 or 2 letters, return nothing
+      if (value.length <= 2) {
+        return [];
+      }
+  
+      // Case 5: 3 or more letters, return city names where any word starts with these 3 letters
+      return data.filter((option: Location) => {
+        const city = option.city ? option.city.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '') : '';
+        return city.split(' ').some(word => word.startsWith(value.toLowerCase()));
+      });
+    }
+  
+    // Default return empty array if no conditions match
+    return [];
+  }
+
+  locationSelected(event : any): void {
+
+    this.isOuSelected = true;
+    this.isTypingOu = false;
+  // The selected option's value
+  const selectedValue = event.option.value;
+
+  // Set the value of 'location' to the selected option
+  this.location = `${selectedValue.postalCode}, ${selectedValue.city}`;
+  }
+
+  // Handle typing in Qui (Who) input
+onQuiInputChange(): void {
+  this.isQuiSelected = false;  // Reset selection when typing
+  this.isSpecialtySelected = false;  // Specialty not selected when typing starts
+
+  // Disable "Où" field while typing in "Qui"
+  this.searchLocationControl.disable();
+  this.location = ''; // Clear the "Où" field
+}
+
+// Handle typing in Ou (Where) input
+onOuInputChange(): void {
+  // If the user starts typing in "Où" without selecting a specialty in "Qui"
+  if (!this.isQuiSelected && !this.isSpecialtySelected) {
+    this.searchTerm = ''; // Clear the "Qui" field if no selection was made
+  }
+}
+
+
+clearQuiField(): void {
+    this.searchTerm = '';  // Clear "Qui" if no valid selection
+  this.searchLocationControl.enable();
+}
+  
+  clearOuField(): void {
+    this.location = '';            // Clear the input value
+    this.isOuSelected = false;     // Reset the selected state
+    this.isTypingOu = false;       // Reset the typing state
+    this.searchControl.enable();   // Re-enable the "Qui" input
+  }
 }
